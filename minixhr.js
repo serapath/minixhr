8495f77b6aed35fbbb945a7d2a61e340e6c25cb9
serapath (Alexander Praetorius) <dev@serapath.de>
2016-02-15 22:34:16 +0100

    REFACTOR comments and var names

--- minixhr.js
+++ minixhr.js
@@ -1,10 +1,8 @@
 var XMLHttpRequest  = require('xhrpolyfill')
 module.exports = function xhr2 (params, callback) {
-  // returns/calls: callback(data, response, xhr)
   var url = typeof params === 'string' ? params : params.url
-  // e.g. url = "http://jsonplaceholder.typicode.com/posts/1"
   var method = params.method || (params.data ? 'POST': 'GET')
-  var body = params.data // data: formdata or {key:val}
+  var body = params.data
   var H = params.headers ? params.headers : params.body ? {
     'X-Requested-With' :'XMLHttpRequest',
     'Content-Type'     :'application/x-www-form-urlencoded'
@@ -12,7 +10,7 @@ module.exports = function xhr2 (params, callback) {
   var xhr = XMLHttpRequest()
   if (!xhr) throw new Error('No AJAX support')
   xhr.open(method, url)
-  for (var field in H) xhr.setRequestHeader(field, H[field])
+  for (var key in H) xhr.setRequestHeader(key, H[key])
   xhr.onload = xhr.onerror = function (response) {
     var Hjson = {}, h = xhr.getAllResponseHeaders()
     ;(h.match(/([^\n\r:]+):([^\n\r]+)/g)||[]).forEach(function(item){
